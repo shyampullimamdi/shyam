523d88986919adcc0b95892f02eca370077d8ac5
shyampullimamdi <shyampullimamdi@gmail.com>
2018-10-30 09:36:06 -0400

    Delete InitializeSliGM.java

--- sli-group-mapper/src/main/java/com/ericsson/eea/ark/sli/grouping/util/InitializeSliGM.java
+++
@@ -1,7 +0,0 @@
-package com.ericsson.eea.ark.sli.grouping.util;
-
-public class InitializeSliGM {
-    public static void main(String[] args) {
-        new InitUpgSliGM(args, false).exec();
-    }
-}
